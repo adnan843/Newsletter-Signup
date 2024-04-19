898a672bd1d026257b70f41e2d0fc42003c0092a
Adnan <adnanakramkhan58@gmail.com>
2024-04-20 00:19:40 +0530

    fifth commit

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post("/failure", function(req,res){
   res.redirect("/");
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log("Server Started at port 3000");
 });
 
